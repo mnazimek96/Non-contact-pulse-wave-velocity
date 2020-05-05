5f95d75e1d3040c3df663cd6851e8454eadd56be
Zimek <mnazimek@student.agh.edu.pl>
2020-05-05 17:05:23 +0200

    Add skin color adjustability and mask
    Added track bar to adjust parameters of image to detect skin color
    Parameters are added to image real time

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 
 import cv2
 import datetime
+import numpy as np
 
 # cam init
 hands_cam0 = cv2.VideoCapture(0)
@@ -25,6 +26,22 @@ out_legs = cv2.VideoWriter(file_name_legs, cv2.VideoWriter_fourcc('M', 'J', 'P',
 start_flag_hands = False
 start_flag_legs = False
 
+
+# slider to adjust image parameters
+def empty(a):
+    pass
+
+
+track_bar_name = "Skin color setup"
+cv2.namedWindow("Skin color setup")
+cv2.resizeWindow(track_bar_name, 640, 240)
+cv2.createTrackbar("Hue Min", track_bar_name, 0, 179, empty)
+cv2.createTrackbar("Hue Max", track_bar_name, 179, 179, empty)
+cv2.createTrackbar("Sat Min", track_bar_name, 0, 255, empty)
+cv2.createTrackbar("Sat Max", track_bar_name, 255, 255, empty)
+cv2.createTrackbar("Val Min", track_bar_name, 0, 255, empty)
+cv2.createTrackbar("Val Max", track_bar_name, 255, 255, empty)
+
 while True:
     cam0_is_connected, hands_frame = hands_cam0.read()
     cam1_is_connected, legs_frame = legs_cam1.read()
@@ -38,6 +55,23 @@ while True:
         # time for synchronise recordings
         now = datetime.datetime.now()
         current_time = now.strftime("%H:%M:%S")
+
+        # making mask due to sliders position
+        frame0HSV = cv2.cvtColor(hands_frame, cv2.COLOR_BGR2HSV)
+
+        h_min = cv2.getTrackbarPos("Hue Min", track_bar_name)
+        h_max = cv2.getTrackbarPos("Hue Max", track_bar_name)
+        s_min = cv2.getTrackbarPos("Sat Min", track_bar_name)
+        s_max = cv2.getTrackbarPos("Sat Max", track_bar_name)
+        v_min = cv2.getTrackbarPos("Val Min", track_bar_name)
+        v_max = cv2.getTrackbarPos("Val Max", track_bar_name)
+        lower = np.array([h_min, s_min, v_min])
+        upper = np.array([h_max, s_max, v_max])
+
+        mask = cv2.inRange(frame0HSV, lower, upper)
+
+        # Adding mask to image
+        hands_frame = cv2.bitwise_and(hands_frame, hands_frame, mask=mask)
 
         # add time stamp
         hands_frame = cv2.putText(hands_frame, current_time, (10, 50), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
@@ -64,6 +98,7 @@ while True:
             start_flag_legs = True
         now = datetime.datetime.now()
         current_time = now.strftime("%H:%M:%S")
+        legs_frame = cv2.bitwise_and(legs_frame, legs_frame, mask=mask)
         legs_frame = cv2.putText(legs_frame, current_time, (10, 50), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
         right_leg_ROI = legs_frame[200:330, 70:200]
         left_leg_ROI = legs_frame[200:330, 440:570]
